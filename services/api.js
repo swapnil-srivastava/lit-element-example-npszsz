7f9bb282b8e01e43424ef6f9600ec2ed1fc78353
swapnil-srivastava <swapnilsrivastava68@hotmail.com>
2020-03-17 16:22:54 +0100

    1 file modified

--- services/api.js
+++ services/api.js
@@ -5,3 +5,15 @@ export function test(id = 1) {
   const updatedTestUrl = `${urls.testUrl}/${id}`;
   return axios.get(updatedTestUrl)
 }
+
+// declare a request interceptor
+axios.interceptors.request.use(config => {
+  // perform a task before the request is sent
+  console.log('Request was sent');
+
+  return config;
+}, error => {
+  // handle the error
+  console.log('Request failed');
+  return Promise.reject(error);
+});
